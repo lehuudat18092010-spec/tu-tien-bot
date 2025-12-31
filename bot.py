419900079b5d70ba01caab6e9485731bcee626d7
lehuudat18092010-spec <lehuudat18092010@gmail.com>
2025-12-31 20:59:31 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,22 @@
+import os
 import discord
 from discord.ext import commands
-import os
 
+# Intents
 intents = discord.Intents.default()
 intents.message_content = True
 
+# Bot
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f"Bot đã online: {bot.user}")
+    print(f"✅ Bot đã online: {bot.user}")
 
+# Lệnh ping
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong 🏓")
+
+# Run bot
 bot.run(os.getenv("TOKEN"))
